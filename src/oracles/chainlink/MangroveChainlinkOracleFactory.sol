d03e9731b0da0019d89467f418775e4a4cbd231c
Maxence Raballand <dev@maxencerb.com>
2024-10-14 17:49:07 +0200

    fix: inverse params chainlink

--- src/oracles/chainlink/MangroveChainlinkOracleFactory.sol
+++ src/oracles/chainlink/MangroveChainlinkOracleFactory.sol
@@ -30,23 +30,23 @@ contract MangroveChainlinkOracleFactory {
   /**
    * @notice Creates a new MangroveChainlinkOracle
    * @param baseFeed1 ChainlinkFeed struct for the first base feed
-   * @param quoteFeed1 ChainlinkFeed struct for the first quote feed
    * @param baseFeed2 ChainlinkFeed struct for the second base feed
+   * @param quoteFeed1 ChainlinkFeed struct for the first quote feed
    * @param quoteFeed2 ChainlinkFeed struct for the second quote feed
    * @param salt Unique value for deterministic address generation
    * @return oracle The newly created MangroveChainlinkOracle
    */
   function create(
     ChainlinkFeed memory baseFeed1,
-    ChainlinkFeed memory quoteFeed1,
     ChainlinkFeed memory baseFeed2,
+    ChainlinkFeed memory quoteFeed1,
     ChainlinkFeed memory quoteFeed2,
     bytes32 salt
   ) external returns (MangroveChainlinkOracle oracle) {
     oracle = new MangroveChainlinkOracle{salt: salt}(
       baseFeed1.feed,
-      quoteFeed1.feed,
       baseFeed2.feed,
+      quoteFeed1.feed,
       quoteFeed2.feed,
       baseFeed1.baseDecimals,
       baseFeed1.quoteDecimals,
