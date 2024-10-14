15dd9e004f5ad310bfb46578515c0282d3a1a1e2
Maxence Raballand <dev@maxencerb.com>
2024-10-14 12:07:33 +0200

    feat: remove lib in factory

--- src/MangroveVaultFactory.sol
+++ src/MangroveVaultFactory.sol
@@ -4,22 +4,6 @@ pragma solidity ^0.8.20;
 import {MangroveVault, AbstractKandelSeeder} from "./MangroveVault.sol";
 import {MangroveVaultEvents} from "./lib/MangroveVaultEvents.sol";
 
-library MangroveVaultFactoryLib {
-  function createVault(
-    AbstractKandelSeeder _seeder,
-    address _BASE,
-    address _QUOTE,
-    uint256 _tickSpacing,
-    uint8 _decimals,
-    string memory name,
-    string memory symbol,
-    address _oracle,
-    address _owner
-  ) external returns (MangroveVault vault) {
-    vault = new MangroveVault(_seeder, _BASE, _QUOTE, _tickSpacing, _decimals, name, symbol, _oracle, _owner);
-  }
-}
-
 contract MangroveVaultFactory {
   function createVault(
     AbstractKandelSeeder _seeder,
@@ -32,9 +16,7 @@ contract MangroveVaultFactory {
     address _oracle,
     address _owner
   ) public returns (MangroveVault vault) {
-    vault = MangroveVaultFactoryLib.createVault(
-      _seeder, _BASE, _QUOTE, _tickSpacing, _decimals, name, symbol, _oracle, _owner
-    );
+    vault = new MangroveVault(_seeder, _BASE, _QUOTE, _tickSpacing, _decimals, name, symbol, _oracle, _owner);
 
     emit MangroveVaultEvents.VaultCreated(
       address(_seeder), _BASE, _QUOTE, _tickSpacing, address(vault), _oracle, address(vault.kandel())
